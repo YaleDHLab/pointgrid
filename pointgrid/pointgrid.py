ddb8d2e246e6ec4998411a0ae48337267848530f
duhaime <douglas.duhaime@gmail.com>
2020-01-10 10:27:23 -0500

    add optional pad param

--- pointgrid/pointgrid.py
+++ pointgrid/pointgrid.py
@@ -3,63 +3,21 @@ import pandas as pd
 import numpy as np
 import time, base64, math
 
-def get_bounds(arr):
-  '''
-  Given a 2D array return the y_min, y_max, x_min, x_max
-  @arg arr numpy.ndarray:
-    a numpy array with shape (n,2)
-  @returns list
-    a list with [y_min, y_max, x_min, x_max]
-  '''
-  return [
-    np.min(arr[:,1]),
-    np.max(arr[:,1]),
-    np.min(arr[:,0]),
-    np.max(arr[:,0]),
-  ]
-
-def create_mesh(arr, h=100, w=100):
-  '''
-  Given a 2D array create a mesh that will hold updated point positions
-  @arg arr numpy.ndarray:
-    a numpy array with shape (n,2)
-  @kwargs h int:
-    the number of unique height positions to create
-  @kwargs w int:
-    the number of unique width positions to create
-  @returns pandas.core.frame.DataFrame
-     dataframe containing the available grid positions
-  '''
-  print(' * creating mesh with size', h, w)
-  bounds = get_bounds(arr)
-  # create array of valid positions
-  y_vals = np.arange(bounds[0], bounds[1], (bounds[1]-bounds[0])/h)
-  x_vals = np.arange(bounds[2], bounds[3], (bounds[3]-bounds[2])/w)
-  # create the dense mesh
-  data = np.tile(
-    [[0, 1], [1, 0]],
-    np.array([
-      int(np.ceil(len(y_vals) / 2)),
-      int(np.ceil(len(x_vals) / 2)),
-    ]))
-  # ensure each axis has an even number of slots
-  if len(y_vals) % 2 != 0 or len(x_vals) % 2 != 0:
-    data = data[0:len(y_vals), 0:len(x_vals)]
-  return pd.DataFrame(data, index=y_vals, columns=x_vals)
-
-
-def align_points_to_grid(arr, fill=0.1, optimal_assignments=False, log_every=None):
+def align_points_to_grid(arr, fill=0.1, pad=0.0, optimal_assignments=False, log_every=None):
   '''
   Snap each point in `arr` to the closest unoccupied slot in a mesh
   @arg arr numpy.ndarray:
     a numpy array with shape (n,2)
-  @kwargs fill float:
+  @kwarg fill float:
     a number 0:1 that indicates what fraction of the grid slots should be
     filled with points
-  @kwargs log_every integer:
+  @kwarg pad float:
+    a decimal value 0:1 that indicates how much padding to add to the border
+    domains to allow jitter positions to move beyond initial data domain
+  @kwarg log_every integer:
     if a positive integer `n` is provided, the function informs the user every
     time `n` more points have been assigned grid positions
-  @kwargs optimal_assignments bool:
+  @kwarg optimal_assignments bool:
     if True assigns each point to its closest open grid point, otherwise an
     approximately optimal open grid point is selected. True requires more
     time to compute
@@ -69,13 +27,13 @@ def align_points_to_grid(arr, fill=0.1, optimal_assignments=False, log_every=Non
   if fill == 0: raise Exception('fill must be greater than 0 and less than 1')
   # create height and width of grid as function of array size and desired fill proportion
   h = w = math.ceil((len(arr)/fill)**(1/2))
+  # find the bounds for the distribution
+  bounds = get_bounds(arr, pad=pad)
   # create the grid mesh
-  grid = create_mesh(arr, h=h, w=w)
+  grid = create_mesh(arr, h=h, w=w, bounds=bounds)
   # fill the mesh
   print(' * filling mesh')
   df = pd.DataFrame(arr, columns=['x', 'y']).copy(deep=True)
-  # find the bounds for the distribution
-  bounds = get_bounds(arr)
   # store the number of points slotted
   c = 0
   for site, point in df.sample(frac=1)[['x', 'y']].iterrows():
@@ -103,6 +61,56 @@ def align_points_to_grid(arr, fill=0.1, optimal_assignments=False, log_every=Non
         if log_every and c % log_every == 0:
           print(' * slotted', c, 'of', len(arr), 'assignments')
   return df.sort_index().to_numpy()
+
+def get_bounds(arr, pad=0.2):
+  '''
+  Given a 2D array return the y_min, y_max, x_min, x_max
+  @arg arr numpy.ndarray:
+    a numpy array with shape (n,2)
+  @kwarg pad float:
+    a decimal value 0:1 that indicates how much padding to add to the border
+    domains to allow jitter positions to move beyond initial data domain
+  @returns list
+    a list with [y_min, y_max, x_min, x_max]
+  '''
+  x_dom = [np.min(arr[:,1]), np.max(arr[:,1])]
+  y_dom = [np.min(arr[:,0]), np.max(arr[:,0])]
+  return [
+    x_dom[0] - np.abs((x_dom[1]-x_dom[0])*pad),
+    x_dom[1] + np.abs((x_dom[1]-x_dom[0])*pad),
+    y_dom[0] - np.abs((y_dom[1]-y_dom[0])*pad),
+    y_dom[1] + np.abs((y_dom[1]-y_dom[0])*pad),
+  ]
+
+def create_mesh(arr, h=100, w=100, bounds=[]):
+  '''
+  Given a 2D array create a mesh that will hold updated point positions
+  @arg arr numpy.ndarray:
+    a numpy array with shape (n,2)
+  @kwarg h int:
+    the number of unique height positions to create
+  @kwarg w int:
+    the number of unique width positions to create
+  @kwarg bounds arr:
+    a list with [y_min, y_max, x_min, x_max]
+  @returns pandas.core.frame.DataFrame
+     dataframe containing the available grid positions
+  '''
+  print(' * creating mesh with size', h, w)
+  # create array of valid positions
+  y_vals = np.arange(bounds[0], bounds[1], (bounds[1]-bounds[0])/h)
+  x_vals = np.arange(bounds[2], bounds[3], (bounds[3]-bounds[2])/w)
+  # create the dense mesh
+  data = np.tile(
+    [[0, 1], [1, 0]],
+    np.array([
+      int(np.ceil(len(y_vals) / 2)),
+      int(np.ceil(len(x_vals) / 2)),
+    ]))
+  # ensure each axis has an even number of slots
+  if len(y_vals) % 2 != 0 or len(x_vals) % 2 != 0:
+    data = data[0:len(y_vals), 0:len(x_vals)]
+  return pd.DataFrame(data, index=y_vals, columns=x_vals)
 
 def _get_grid_location(grid, point, r_x, r_y, optimal_assignments=False):
   '''
@@ -137,7 +145,6 @@ def _get_grid_location(grid, point, r_x, r_y, optimal_assignments=False):
       if val != 1: continue
       if not optimal_assignments:
         return [x, y]
-        assigned = True
       else:
         dist = euclidean(point, (x,y))
         if dist < best_dist:
